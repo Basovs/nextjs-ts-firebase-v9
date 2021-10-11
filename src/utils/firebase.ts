c3faa7a3a2c11f2a654dd5da18537f4339ae60f4
Janis Basis Basovs <janis_basis@hotmail.com>
2021-10-11 09:16:29 +0300

    Add some general CRUD for firebase

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,11 +1,69 @@
-import { addDoc, collection } from "@firebase/firestore"
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  deleteField,
+  doc,
+  setDoc,
+  updateDoc,
+} from "@firebase/firestore"
 import db from "../firebase/client"
 
-// create doc
-export const createDoc = async (collectionName: string, payload: object) => {
-  console.log("Creating doc")
+// set doc with specific ID
+// OBS setDoc is dangerous - can owerwrite whole doc
+export const createDocWithSpecificID = async (
+  collectionName: string,
+  payload: object,
+  id: string
+) => {
+  console.log("Setting doc")
+
+  await setDoc(doc(db, collectionName, id), payload)
+}
+
+// set doc with specific ID with 'merge' option to
+// prevent overwrite of whole doc
+export const mergeDocWithSpecificID = async (
+  collectionName: string,
+  payload: object,
+  id: string
+) => {
+  console.log("Setting doc")
+
+  await setDoc(doc(db, collectionName, id), payload)
+}
+
+// add doc with firebase auto ID
+export const createDocWithAutoID = async (
+  collectionName: string,
+  payload: object
+) => {
+  console.log("Adding doc")
 
   const collectionRef = collection(db, collectionName)
   const newDocRef = await addDoc(collectionRef, payload)
-  console.log("The new doc ID is: ", newDocRef.id)
+  // console.log("The new doc ID is: ", newDocRef.id)
+  return newDocRef
+}
+
+// delete doc
+export const deleteItem = async (collectionName: string, id: string) => {
+  console.log("Deleting doc")
+
+  await deleteDoc(doc(db, collectionName, id))
+}
+
+// delete specific doc field
+export const deleteDocField = async (
+  collectionName: string,
+  id: string,
+  fieldName: string
+) => {
+  console.log("Deleting doc field")
+
+  const docRef = doc(db, collectionName, id)
+
+  await updateDoc(docRef, {
+    [fieldName]: deleteField(),
+  })
 }
